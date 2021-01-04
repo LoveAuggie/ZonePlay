a074cc60d5af94fd4fc07ae8de16d24f041e076c
LoveAuggie <381724359@qq.com>
2021-01-04 08:48:16 +0800

    深度迷宫

---
+++ src/ZonePlay/Zone/Zone2.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZonePlay.Zone
+{
+    /// <summary>
+    /// 随机Prim
+    /// </summary>
+    public class Zone2 : BaseZone
+    {
+        /*1.让迷宫全是墙.
+         *2.选一个单元格作为迷宫的通路，然后把它的邻墙放入列表
+         *3.当列表里还有墙时
+         *	1.从列表里随机选一个墙，如果这面墙分隔的两个单元格只有一个单元格被访问过
+         *		1.那就从列表里移除这面墙，即把墙打通，让未访问的单元格成为迷宫的通路
+         *		2.把这个格子的墙加入列表
+         *	2.如果墙两面的单元格都已经被访问过，那就从列表里移除这面墙
+         */
+
+        public Zone2(int width, int height)
+            : base(width, height)
+        {
+            // 将迷宫全部设置为墙
+            for (int i = 1; i <= width; i++)
+            {
+                for (int j = 1; j <= height; j++)
+                {
+                    ZoneArr[i, j] = 1;
+                }
+            }
+            CreateZone();
+        }
+
+        protected override void CreateZone()
+        {
+            var start = new Point(1, 1);
+            List<Point> wList = new List<Point>();
+            ZoneArr[start.X, start.Y] = 0;
+            var list = GetNexWall(start);
+            wList.AddRange(list);
+            while (wList.Count > 0)
+            {
+                // 随机选一个墙
+                var wall = wList[rand.Next(wList.Count)];
+
+                // 分隔的单元格？上下左右都计算一下吧
+                int s = ZoneArr[wall.X, wall.Y - 1];
+                int x = ZoneArr[wall.X, wall.Y + 1];
+                int z = ZoneArr[wall.X - 1, wall.Y];
+                int y = ZoneArr[wall.X + 1, wall.Y];
+
+                wList.Remove(wall); // 先移除，再判断是否需要掏空
+                if (s + x == 1)
+                {
+                    ZoneArr[wall.X, wall.Y] = 0; // 先掏空墙，再掏空对面的点
+                    if (s == 1)
+                    {
+                        ZoneArr[wall.X, wall.Y - 1] = 0;
+                        wList.AddRange(GetNexWall(new Point(wall.X, wall.Y - 1)));
+                    }
+                    else
+                    {
+                        ZoneArr[wall.X, wall.Y + 1] = 0;
+                        wList.AddRange(GetNexWall(new Point(wall.X, wall.Y + 1)));
+                    }
+                }
+                else if (z + y == 1)
+                {
+                    ZoneArr[wall.X, wall.Y] = 0;
+                    if (z == 1)
+                    {
+                        ZoneArr[wall.X - 1, wall.Y] = 0;
+                        wList.AddRange(GetNexWall(new Point(wall.X - 1, wall.Y)));
+                    }
+                    else
+                    {
+                        ZoneArr[wall.X + 1, wall.Y] = 0;
+                        wList.AddRange(GetNexWall(new Point(wall.X + 1, wall.Y)));
+                    }
+                }
+            }
+        }
+
+        private List<Point> GetNexWall(Point p)
+        {
+            List<Point> list = new List<Point>();
+
+            var sp = new Point(p.X, p.Y - 1);
+            if (CheckPoint(sp, 1))
+                list.Add(sp);
+
+            sp = new Point(p.X, p.Y + 1);
+            if (CheckPoint(sp, 1))
+                list.Add(sp);
+
+            sp = new Point(p.X - 1, p.Y);
+            if (CheckPoint(sp, 1))
+                list.Add(sp);
+
+            sp = new Point(p.X + 1, p.Y);
+            if (CheckPoint(sp, 1))
+                list.Add(sp);
+            
+            return list;
+        }
+    }
+}
---
+++ src/ZonePlay/Zone/Zone3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZonePlay.Zone
+{
+    /// <summary>
+    /// 递归分割
+    /// </summary>
+    public class Zone3 : BaseZone
+    {
+        /*就是把空间用十字分成四个子空间，然后在三面墙上挖洞（为了确保连通），
+         * 之后对每个子空间继续做这件事直到空间不足以继续分割为止
+         */
+
+        public Zone3(int width, int height)
+            : base(width, height)
+        {
+            CreateZone();
+        }
+
+        protected override void CreateZone()
+        {
+            Cross(0, zWidth, 0, zHeight);
+        }
+
+        private void Cross(int xST, int xEd, int yST, int yEd)
+        {
+            // 在一定的空间内，划一个十字墙，然后四条边上，随机挖一个洞
+            throw new NotImplementedException();
+        }
+    }
+}
